d30d583d3626c734b25fcf11e9b11f145485693e
sazarina <sazarina500@gmail.com>
2017-04-29 15:53:16 +0900

    プレイヤーの移動を実装しました

--- Assets/Scrpts/Player.cs
+++ Assets/Scrpts/Player.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public Rigidbody rig;
+    public Vector2 position;
+    // Use this for initialization
+    void Start() {
+        //rig = GetComponent<Rigidbody>();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update() {
+        position = transform.position;
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rig.AddForce(new Vector2(0.0f, 200.0f));
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            position.x += 0.1f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            //rig.AddForce(new Vector2(-10.0f, 0.0f));
+            position.x -= 0.1f;
+        }
+        transform.position = position;
+
+    }
 }
