c234975551679537ab04689b6e9f316569fe64b2
sazarina <sazarina500@gmail.com>
2017-04-29 21:44:05 +0900

    マップのランダム生成を調整

--- Assets/Scrpts/StageController.cs
+++ Assets/Scrpts/StageController.cs
@@ -6,18 +6,20 @@ public class StageController : MonoBehaviour {
     int[,] stageData = new int[100, 10];
 
     public GameObject stage;
-    int marginX = 0, marginY = 1;
+    int marginX = -1, marginY = 1;
     // Use this for initialization
     void Start() {
         for (int x = 0; x < stageData.GetLength(0); x++) {
             for (int y = 0; y < stageData.GetLength(1) - 1; y++) {
                 stageData[x, y] = Random.Range(0, 1 + 1);
+                stageData[0, y] = 1;
+                stageData[stageData.GetLength(0)-1, y] = 1;
+                stageData[x, 0] = 1;
+                stageData[x, 3] = 0;
+                stageData[x, 2] = 0;
+                stageData[x, 1] = 0;
                 Debug.Log(stageData[x, y]);
             }
-            stageData[x, 3] = 0;
-            stageData[x, 2] = 0;
-            stageData[x, 1] = 0;
-            stageData[x, 0] = 1;
         }
         for (int x = 0; x < stageData.GetLength(0); x++) {
             for (int y = 0; y < stageData.GetLength(1); y++) {
