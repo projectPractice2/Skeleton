66f5762a082f21b40c0573fb14993af3f9da598b
sazarina <sazarina500@gmail.com>
2017-05-20 21:52:07 +0900

    スマホ用のボタン(デバッグ)とフリック

---
+++ Assets/Scrpts/Key.cs
@@ -0,0 +1,36 @@
+﻿﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+    public Dictionary<string, bool> keys = new Dictionary<string, bool>();
+    // Use this for initialization
+    void Start() {
+        keys.Add("Up", false);
+        keys.Add("Down", false);
+        keys.Add("Left", false);
+        keys.Add("Right", false);
+        keys.Add("Jump", false);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    
+    }
+
+    public bool IsKey(string key){
+        bool flag = false;
+        if (keys.ContainsKey(key)) {
+            flag = keys[key];
+        }
+        else{
+            Debug.Log("dont contain "+ key);
+        }
+        return flag;
+    }
+
+    public void SetKey(string key, bool flag) {
+        keys[key] = flag;
+    }
+}
---
+++ Assets/Scrpts/SlidableButton.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Image))]
+public class SlidableButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    Sprite notPressedSprite;
+    public Sprite pressedSprite;
+    Key key;
+
+    Image image;
+
+    bool isButtonDown = false;
+
+    bool isPointerDown = false;
+    bool isPointerIn = false;
+
+    public UnityEvent onButtonDown;
+    public UnityEvent onButtonUp;
+
+    string mKey;
+
+    Player player;
+
+    public void Start() {
+        key = GameObject.Find("Key").GetComponent<Key>();
+        mKey = gameObject.name;
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        image = GetComponent<Image>();
+        notPressedSprite = image.sprite;
+    }
+
+    void UpdateState() {
+        if (!isPointerDown || !isPointerIn) {
+            image.sprite = notPressedSprite;
+            if (isButtonDown) {
+                onButtonUp.Invoke();
+                isButtonDown = false;
+            }
+            return;
+        }
+        image.sprite = pressedSprite;
+        if (!isButtonDown) {
+            onButtonDown.Invoke();
+            isButtonDown = true;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData ev) {
+        isPointerIn = true;
+        UpdateState();
+        Debug.Log("Enter");
+        key.keys[mKey] = true;
+    }
+
+    public void OnPointerExit(PointerEventData ev) {
+        isPointerIn = false;
+        UpdateState();
+        //key.SetKey(mKey, false);
+        //Debug.Log("Exit");
+        key.keys[mKey] = false;
+    }
+
+    public void OnGroupPointerDown() {
+        isPointerDown = true;
+        Debug.Log("Down");
+        UpdateState();
+    }
+
+    public void OnGroupPointerUp() {
+        isPointerDown = false;
+        Debug.Log("Up");
+        UpdateState();
+    }
+}
