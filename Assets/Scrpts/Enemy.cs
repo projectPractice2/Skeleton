84416fbe24ab90b275f0318b97067c83beb4481b
sazarina <sazarina500@gmail.com>
2017-05-30 16:20:00 +0900

    Enemyのプレハブとコントローラーオブジェクトの作成

---
+++ Assets/Scrpts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    int attack, hp;
+    float speed = 0.5f;
+
+    Rigidbody rig;
+
+	// Use this for initialization
+	void Start () {
+        rig = GetComponent<Rigidbody>();
+        rig.velocity = new Vector3(-0.1f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void Move(){
+        
+    }
+
+    public void Create(int attack,int hp){
+        this.attack = attack;
+        this.hp = hp;
+    }
+}
---
+++ Assets/Scrpts/EnemyController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ジェネリックを隠すために継承してしまう
+/// [System.Serializable]を書くのを忘れない
+/// </summary>
+[System.Serializable]
+public class EnemyTable : Serialize.TableBase<string, GameObject, EnemyPair> {
+}
+
+/// <summary>
+/// ジェネリックを隠すために継承してしまう
+/// [System.Serializable]を書くのを忘れない
+/// </summary>
+/// 
+[System.Serializable]
+public class EnemyPair : Serialize.KeyAndValue<string, GameObject> {
+    public EnemyPair(string key, GameObject value) : base(key, value) {
+    }
+}
+
+public class EnemyController : MonoBehaviour {
+    int cnt = 0;
+
+    [SerializeField]
+    EnemyTable enemyDict;
+
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //if (cnt % 2 == 0) {
+            EncountEnemy();
+        EncountEnemy();
+        EncountEnemy();
+        //}
+        cnt++;
+    }
+    void EncountEnemy() {
+        var obj = Instantiate(enemyDict.GetTable()["Enemy"], new Vector2(90.0f, 6.5f), Quaternion.identity);
+        obj.GetComponent<Rigidbody>().velocity = new Vector2(-20.0f, 0);
+    }
+}
