8c7e79bf1127e2a95435691a8294d1821c2b1a82
sazarina <sazarina500@gmail.com>
2017-05-30 12:34:52 +0900

    2段ジャンプの実装

--- Assets/Scrpts/Player.cs
+++ Assets/Scrpts/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour {
     public Vector2 position;
     public Vector2 offset;
     public float speed;
+    int jumpCnt = 0;
 
     float StartPos, EndPos;//x座標
     Vector2 pos;
@@ -37,23 +38,26 @@ public class Player : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         position = transform.position;
-        if ((Input.GetKeyDown(KeyCode.Space) || key.IsKey("Jump")) && transform.position.y <= 2.5) {
+        if ((Input.GetKeyDown(KeyCode.Space) || key.IsKey("Jump")) && jumpCnt <= 10) {
             rig.AddForce(new Vector2(0.0f, 5000.0f));
-        }
-        if(Input.GetKey(KeyCode.DownArrow)){
-            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        } else{
-            transform.localScale = new Vector3(1.0f, 2.0f, 1.0f);
-        }
+            jumpCnt++;
+        } else if (transform.position.y <= 2.5)
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            } else {
+                transform.localScale = new Vector3(1.0f, 2.0f, 1.0f);
+            }
         if (Input.GetKey(KeyCode.RightArrow) || key.IsKey("Right")) {
-            position.x += 0.1f * speed;
+            //position.x += 0.1f * speed;
+            rig.AddForce(new Vector2(50f * speed, 0));
             speed += 0.05f;
         }
         if (Input.GetKey(KeyCode.LeftArrow) || key.IsKey("Left")) {
-            position.x -= 0.1f * speed;
+            rig.AddForce(new Vector2(-50f * speed, 0));
+            //position.x -= 0.1f * speed;
             speed -= 0.05f;
         } else {
-            position.x += 0.1f * speed;
+            //position.x += 0.1f * speed;
         }
         Flick();
         transform.position = position;
@@ -72,6 +76,12 @@ public class Player : MonoBehaviour {
             position.x -= 0.1f;
         } else if (StartPos < EndPos) {
             position.x += 0.1f;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "block") {
+            jumpCnt = 0;
         }
     }
 }
