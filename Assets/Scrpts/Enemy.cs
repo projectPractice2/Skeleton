c602b903f56eab6a5eadccdbbd1ed91a769f8c3e
sazarina <sazarina500@gmail.com>
2017-06-14 19:15:02 +0900

    ちょっとした変更

--- Assets/Scrpts/Enemy.cs
+++ Assets/Scrpts/Enemy.cs
@@ -8,38 +8,38 @@ public class Enemy : MonoBehaviour {
 
     Rigidbody rig;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         stageController = GameObject.Find("StageController").GetComponent<StageController>();
         position = transform.position;
         rig = GetComponent<Rigidbody>();
         //rig.velocity = new Vector3(-20f, 0);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        rig.AddForce(new Vector2(-20.0f*speed, 0));
-        AA();
-	}
+    }
 
-    void Move(){
-        
+    // Update is called once per frame
+    void Update() {
+        rig.AddForce(new Vector2(-20.0f * speed, 0));
+        AA();
+    }
+
+    void Move() {
+
     }
 
     void AA() {
         int x = Mathf.FloorToInt(transform.position.x);
         int y = Mathf.FloorToInt(transform.position.y);
-        if (x <= 0 || x > 94  || y<0 || y >= 30) {
+        if (x <= 0 || x > 94 || y < 0 || y >= 30) {
             Destroy(gameObject);
         }
-        Debug.Log("x : " + x + " , y : " + y);
+        //        Debug.Log("x : " + x + " , y : " + y);
         //Debug.Log("stageData:"+stageController.stageData[y + 1][x - 1]);
         if (stageController.stageData[y][x - 1] == 9 && x != 0) {
             rig.AddForce(new Vector2(0, 300f));
         }
     }
 
-    public void Create(int attack,int hp){
+    public void Create(int attack, int hp) {
         this.attack = attack;
         this.hp = hp;
     }
